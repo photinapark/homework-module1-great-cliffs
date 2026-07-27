167e194cb53edc5faabb200787f57cc69bc837aa
photinapark <photina0146@gmail.com>
2026-07-26 21:32:50 -0700

    Complete Week 4 Day 2 homework

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,11 +1,11 @@
 /* ------- RANDOM CODES ------- */
 
-//Function to generate combination of characters
-function generateCode() {
-    //Create variables to store generated codes and the type of characters we want to show as codes
-    var code = " "; //initialize to null value
+    var code = ""; //to store generated codes and initialize to empty value
+    var getCode = ""; //to store entered code
+    var btnvalue; //for button boolean value
     var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@$";
 
+    function generateCode() {
     //Generate character multiple times using a loop
     for (i = 1; i <= 8; i++) {
         var char = Math.random()* str.length;
@@ -18,9 +18,27 @@ function generateCode() {
 document.getElementById("codes").innerHTML = generateCode();
 
 //Disable Button
-function disableButton() {
-    document.getElementById("submit").disabled = true
+function disableButton(btnvalue) {
+    document.getElementById("submit").disabled = btnvalue;
+    if (btnvalue == true) {
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+    } else {
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+    }
 }
 
-//Activate function
-disableButton();
+//listen to user input code
+var codebox = document.getElementById("codeentered")
+codebox.addEventListener("input", evaluateCode);
+
+//run function if detected user had entered a character in textbox
+function evaluateCode() {
+    getCode = document.getElementById("codeentered").value;
+    var charset1 = getCode.trim();
+    var charset2 = code.trim();
+    if (charset1.length == charset2.length && charset1 == charset2) {
+        disableButton(false)
+    }
+}
