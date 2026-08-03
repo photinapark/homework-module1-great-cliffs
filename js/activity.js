df43cd782b5541d630b911cf7bd718767717ea11
photinapark <photina0146@gmail.com>
2026-08-02 20:18:39 -0700

    add activity selection display feature

--- js/activity.js
+++ js/activity.js
@@ -12,3 +12,30 @@ $(".selectable").click(function(){
 $(".selectable").hover(function(){
     $(this).css("cursor", "pointer");
 });
+
+//User interaction with the table cells
+$(document).ready(function(){
+    $("td").click(function(){
+        var content = $(this).text();
+
+        if(content != "Not Available") {
+            $(this).toggleClass("tdhighlight");
+
+        if($(this).hasClass("tdhighlight")) {
+        $('#displaySelected').css("visibility", "visible");
+        $('#displaySelected').css("margin-top", "2em");
+        var site = $(this).index();
+        var cliff = $("thead th").eq(site).text();
+        $('#result').append("<p>" + content + " at " + cliff + "</p>");
+
+        } else {
+            $('#result p:contains('+content+')').remove();
+
+            if ($('#result').has('p').length == false) {
+                $('#displaySelected').css("visibility", "hidden");
+                $('#displaySelected').css("margin-top", "0em");
+            }
+        }
+    }
+    });
+});
